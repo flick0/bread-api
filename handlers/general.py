79ec65f2688c4e6e62345fc5f33a67c2498cb47e
Russian-Dev <65181690+Russian-Dev@users.noreply.github.com>
2022-01-29 21:55:28 -0600

    Update general.py

--- handlers/general.py
+++ handlers/general.py
@@ -4,6 +4,11 @@ from aiohttp import web
 class general(web.Application):
     def __init__(self, app):
         self.app = app
+    
+    async def page(self,request):
+      return web.FileResponse(path="./static/index.html") 
+
+
 
     async def ping(self, request):
         rs = {"success": True, "data": "pong!"}
@@ -11,4 +16,4 @@ class general(web.Application):
 
 
 def setup(app):
-    return general(app), {"ping":"/ping"}
+    return general(app), {"ping":"/ping","page":"/"}
